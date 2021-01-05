5a69b4e1ae985e6f8b4783364e91d75e9c6e3665
Nicholas Merolle <nmerolle11@hotmail.com>
2021-01-05 04:11:01 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,11 +3,11 @@ class Student
 
   def self.new_from_db(row)
     def self.new_from_db(row)
-  new_student = self.new  # self.new is the same as running Song.new
+  new_student = self.new 
   new_student.id = row[0]
   new_student.name =  row[1]
-  new_student.length = row[2]
-  new_song  # return the newly created instance
+  new_student.grade = row[2]
+  new_student 
 end
   end
 
