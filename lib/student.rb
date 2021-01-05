f35409ce2f3d8f0fc2fca554f3c1454f2e4f8b31
Nicholas Merolle <nmerolle11@hotmail.com>
2021-01-05 05:11:31 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -112,8 +112,12 @@ class Student
     end.first
   end
   
-  def self.all_students_in_grade_X
-    
+  def self.all_students_in_grade_X(grade)
+        sql = <<-SQL
+       SELECT * FROM students WHERE grade = ?
+    SQL
+
+    DB[:conn].execute(sql, grade)
     
     
   end
