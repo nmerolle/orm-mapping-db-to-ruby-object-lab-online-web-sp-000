fd6227338fc1ec21874df421f408bc4dca0c1fd4
Nicholas Merolle <nmerolle11@hotmail.com>
2021-01-05 04:47:26 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -73,6 +73,19 @@ class Student
     end
   end
   
+    def self.all_students_in_grade_9
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade <= 9
+      LIMIT ?
+    SQL
+ 
+    DB[:conn].execute(sql, 9).map do |row|
+      self.new_from_db(row)
+    end
+  end
+  
   
   
 end
